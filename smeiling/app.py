652795699d4df3e869751374244e5e5667461fa9
Mark Douthwaite <mark@douthwaite.io>
2019-11-16 23:48:35 +0000

    ssl activated for k8s

--- smeiling/app.py
+++ smeiling/app.py
@@ -1,13 +1,16 @@
+import yaml
 from flask import Flask, render_template
 
 
-SSL_ENABLED = False
+CONFIG = yaml.load(open("config/config.yml"), Loader=yaml.SafeLoader)["app"]
 
+
+PROXY_SCHEME = "https" if CONFIG.get("ssl", False) else "http"
+PROXY_HOST = CONFIG["host"]
+PROXY_PORT = CONFIG["port"]
 
 app = Flask(__name__, template_folder="templates")
 
-
 @app.route("/")
 def smeiling():
-    schema = "https" if SSL_ENABLED else "http"
-    return render_template("index.html", target=f"{schema}://http://130.211.57.126/_login?dashboard=Vodafone")
+    return render_template("index.html", target=f"{PROXY_SCHEME}://{PROXY_HOST}:{PROXY_PORT}/_login?dashboard=Vodafone")
--- splunk/settings.py
+++ splunk/settings.py
@@ -2,15 +2,15 @@ import yaml
 
 SSL_ENABLED = False
 
-CONF = yaml.load(open("config/config.yml"), Loader=yaml.SafeLoader)
+CONFIG = yaml.load(open("config/config.yml"), Loader=yaml.SafeLoader)["app"]
 
 # Get dashboards
-DASHBOARDS = CONF["webapp"]["dashboards"]
+DASHBOARDS = CONFIG["dashboards"]
 
 # Setup Splunk config
-SPLUNK = CONF["webapp"]["splunk"]
+SPLUNK = CONFIG["proxy"]
 
-SPLUNK_SCHEME = "http"
+SPLUNK_SCHEME = SPLUNK["scheme"]
 SPLUNK_USERNAME = SPLUNK["username"]
 SPLUNK_PASSWORD = SPLUNK["password"]
 SPLUNK_HOST = SPLUNK["host"]
@@ -19,7 +19,7 @@ SPLUNK_PORT = SPLUNK["port"]
 SPLUNK_BASE = f"{SPLUNK_SCHEME}://{SPLUNK_HOST}:{SPLUNK_PORT}"
 
 # Setup proxy config
-PROXY_SCHEME = "https" if SSL_ENABLED else "http"
-PROXY_PORT = 80
-PROXY_HOST = "130.211.57.126"
+PROXY_SCHEME = "https" if CONFIG.get("ssl", False) else "http"
+PROXY_PORT = CONFIG["port"]
+PROXY_HOST = CONFIG["host"]
 PROXY_BASE = f"{PROXY_SCHEME}://{PROXY_HOST}:{PROXY_PORT}"
